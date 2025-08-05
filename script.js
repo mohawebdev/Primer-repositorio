14d3013113e0eac1e0702f41bb7ef37be8a32bc3
Mohamed Maghzaz <111018800+mohawebdev@users.noreply.github.com>
2025-08-05 10:09:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,32 +20,44 @@
 
 /***************ContactForm**********************/
 
-((d)=>{
-const $form= d.querySelector('.contact-form'),
-$loader= d.querySelector('.contact-form-loader'),
-$response= d.querySelector('.contact-form-response');
-$form.addEventListener('submit', (e)=>{
-      e.preventDefault();
-      $loader.classList.remove('none');
-      fetch('https://formsubmit.co/ajax/cebf5e0d9214ce59eb61f266ce12f327', {
-            method: 'POST',
-            body: new FormData(e.target),
-      })
-      .then((res)=> (res.ok? res.json(): Promise.reject(res)))
-      .then((json)=> {
-            console.log(json);
-            location.hash= '#gracias';
-            $form.reset();
-      })
-      .catch(err=>{
-            console.log(err);
-            let message= err.statusText || 'Ocurrió un error al enviar, intenta nuevamente.'
-            $response.querySelector('h3').innerHTML= `Error ${err.status}: ${message}`;
-      }).finally(()=>{
-            $loader.classList.add('none');
-            setTimeout(()=>{
-                  location.hash= '#close';
-            }, 3000);
-      });
-});
+((d) => {
+  const $form = d.querySelector('.contact-form'),
+        $loader = d.querySelector('.contact-form-loader'),
+        $response = d.querySelector('.contact-form-response'),
+        $submitBtn = $form.querySelector('button[type="submit"]');
+
+  $form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    $loader.classList.remove('none');
+    $submitBtn.disabled = true;
+
+    fetch('https://formsubmit.co/ajax/cebf5e0d9214ce59eb61f266ce12f327', {
+      method: 'POST',
+      body: new FormData(e.target),
+    })
+    .then((res) => res.ok ? res.json() : Promise.reject(res))
+    .then((json) => {
+      console.log(json);
+      location.hash = '#gracias';
+      $form.reset();
+    })
+    .catch(err => {
+      console.error(err);
+      if (err instanceof Response) {
+        err.text().then(msg => {
+          $response.querySelector('h3').innerHTML = `Error ${err.status}: ${err.statusText || msg}`;
+        });
+      } else {
+        $response.querySelector('h3').innerHTML = `Error: Ocurrió un problema de conexión o del navegador.`;
+      }
+    })
+    .finally(() => {
+      $loader.classList.add('none');
+      $submitBtn.disabled = false;
+      setTimeout(() => {
+        location.hash = '#close';
+      }, 3000);
+    });
+  });
 })(document);
+
